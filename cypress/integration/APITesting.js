fec88e7b41155b91e5265b3b785e7a299fd3576f
aimtrettel <aimeetrettle@MXRadmins-MacBook-Pro.local>
2021-01-20 16:39:59 -0500

    Fixed APITesting.js

--- cypress/integration/APITesting.js
+++ cypress/integration/APITesting.js
@@ -4,42 +4,61 @@ let innerJson, method, endpoint, query, good, parameters, request, keys;
 
 describe('Automated tests for the omar-superoverlay API endpoints', () => {
     tests.forEach((test) => {
-        innerJson = json.tests[test];
-        method = innerJson["method"];
-        endpoint = innerJson["endpoint"];
-        query = innerJson["in"] === "query";
-        good = innerJson["expected"] === "good";
-        parameters = Object.keys(innerJson["parameters"]);
-        if(query && good) {
-            request = "?"
-            parameters.forEach((parameter) => {
-                request = request + parameter + "=" + innerJson.parameters[parameter] + "&";
-            })
-            request = request.substring(0, request.length - 1);
-            it(`Should test 200 code for ${test} test values`, () => {
-                cy.request(method, endpoint + request)
-                    .then((response) => {
-                        expect(response.status).to.eq(200)
+            it(`Should test ${test}`, () => {
+                innerJson = json.tests[test]
+                method = innerJson["method"];
+                endpoint = innerJson["endpoint"];
+                query = innerJson["in"] === "query";
+                good = innerJson["expected"] === "good";
+                parameters = Object.keys(innerJson["parameters"]);
+                if(query && good) {
+                    request = "?"
+                    parameters.forEach((parameter) => {
+                        request = request + parameter + "=" + innerJson.parameters[parameter] + "&";
                     })
-            })
-            it(`Should test response header for ${test}`, () => {
-                cy.request(method, endpoint + request)
-                    .then((response) => {
-                        expect(response).to.have.property("headers")
+                    request = request.substring(0, request.length - 1);
+                    cy.request({method: method, url: endpoint + request, failOnStatusCode: false})
+                        .then((response) => {
+                            expect(response.status).to.eq(200)
+                        })
+                }
+                else if(query) {
+                    request = "?"
+                    parameters.forEach((parameter) => {
+                        request = request + parameter + "=" + innerJson.parameters[parameter] + "&";
                     })
+                    request = request.substring(0, request.length - 1);
+                    cy.request({method: method, url: endpoint + request, failOnStatusCode: false})
+                        .then((response) => {
+                            expect(response.status).to.not.eq(200)
+                    })
+                }
+                else if(good) {
+                    cy.request({method: method, url: endpoint, body: innerJson.parameters["body"], failOnStatusCode: false})
+                        .then((response) => {
+                            expect(response.status).to.eq(200)
+                    })
+                }
             })
-        }
-        else if(query) {
-            request = "?"
-            parameters.forEach((parameter) => {
-                request = request + parameter + "=" + innerJson.parameters[parameter] + "&";
+            it(`Should test the header of ${test}`, () => {
+                innerJson = json.tests[test]
+                method = innerJson["method"];
+                endpoint = innerJson["endpoint"];
+                query = innerJson["in"] === "query";
+                good = innerJson["expected"] === "good";
+                parameters = Object.keys(innerJson["parameters"]);
+                if(query) {
+                    cy.request({method: method, url: endpoint + request, failOnStatusCode: false})
+                        .then((response) => {
+                            expect(response).to.have.property("headers")
+                        })
+                }
+                else {
+                    cy.request({method: method, url: endpoint, body: innerJson.parameters["body"], failOnStatusCode: false})
+                        .then((response) => {
+                            expect(response).to.have.property("headers")
+                        })
+                }
             })
-            request = request.substring(0, request.length - 1);
-            it(`Should test for a not 200 code for ${test} test values`, () => {
-                cy.request(method, endpoint + request).then((response) => {
-                    expect(response.status).to.not.eq(200)
-                })
-            })
-        }
     })
 })
