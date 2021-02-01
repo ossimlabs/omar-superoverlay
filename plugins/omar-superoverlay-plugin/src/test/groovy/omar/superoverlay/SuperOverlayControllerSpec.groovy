cad0ee5362f5d25261b0a8dc6c8d7ffb9d70d20e
Craig Schneider <craig.schneider@maxar.com>
2021-02-01 17:40:13 -0500

    rename test folder

--- plugins/omar-superoverlay-plugin/src/test/groovy/omar/superoverlay/SuperOverlayControllerSpec.groovy
+++ plugins/omar-superoverlay-plugin/src/test/groovy/omar/superoverlay/SuperOverlayControllerSpec.groovy
@@ -3,6 +3,16 @@ package omar.superoverlay
 import spock.lang.Specification
 
 class SuperOverlayControllerSpec extends Specification {
+
+
+
     void setup() {
+    }
+
+    void 'test kmlQuery maxFeatures exceeds upper bounds'() {
+
+    }
+    void 'test kmlQuery maxFeatures exceeds lower bounds'() {
+
     }
 }
